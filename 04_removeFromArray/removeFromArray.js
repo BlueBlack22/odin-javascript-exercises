77e758b906942f24f816004b62afb744aaec8141
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-20 18:51:58 +0200

    Finished 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...itemsToRemove) {
+    for (let i = 0; i < itemsToRemove.length; i++) {
+        inputArray = inputArray.filter((item) => item !== itemsToRemove[i]);
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+const removeFromArray = function(inputArray, itemToRemove) {
+    return result = inputArray.filter((item) => item !== itemToRemove)
+};
+*/
