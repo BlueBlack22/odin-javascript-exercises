c57074f881b07a8ad531e16fda24a0ff56aa5c56
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-20 19:09:54 +0200

    Finished 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    if (firstNum < 0 || secondNum < 0) return "ERROR";
+    if (firstNum > secondNum) {
+        const tempNum = firstNum;
+        firstNum = secondNum;
+        secondNum = tempNum;
+    }
 
+    let sum = 0;
+
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum  += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
