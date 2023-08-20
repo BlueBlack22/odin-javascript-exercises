e3849b98117c9a2df7ce18eeb70c77d3d3c2c19e
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-20 17:41:09 +0200

    Finished 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    const stringLength = inputString.length;
+    let tempString = [];
+    let outputString = "";
+    for (i = 0; i < stringLength; i++) {
+        tempString[i] = inputString.charAt(i);
+    }
 
+    for (i = stringLength - 1; i >= 0; i--) {
+        outputString += tempString[i];
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
