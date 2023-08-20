4d5ab7fddcd7df24db0ad2ac2be9e897ec572756
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-20 19:27:51 +0200

    Finished 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+function roundToOneDecimal (temp) {
+  return Math.round(temp * 10) / 10;
+}
+
+const convertToCelsius = function(tempInF) {
+  const tempInC = ((tempInF - 32) * 5) / 9;
+  
+  if (Number.isInteger(tempInC)) {
+    return tempInC;
+  }
+  return roundToOneDecimal(tempInC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  const tempInF = ((tempInC * 9) / 5) + 32;
+  
+  if (Number.isInteger(tempInF)) {
+    return tempInF;
+  }
+  return roundToOneDecimal(tempInF);
 };
 
 // Do not edit below this line
