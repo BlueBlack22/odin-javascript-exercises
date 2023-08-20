26baa7b2748f46b1bf1752e63ed8521be9ad2e86
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-20 17:31:06 +0200

    Finished 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+    let outputString = "";
+    
+    if (repetitions < 0) {
+        outputString = "ERROR"
+    } else {
+        for (let i = 1; i <= repetitions; i++) {
+            outputString += string;
+        }
+    }
 
+    return outputString;
 };
 
 // Do not edit below this line
