404265fdef2fbb204179ec00ee70525ef8e8efc9
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-22 16:26:17 +0200

    Finished 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    if (n === 0) return 0;
 
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+
+    for (let i = 2; i <= n; i++) {
+        let current = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = current;        
+    }
+
+    return firstPrevious;
 };
 
 // Do not edit below this line
