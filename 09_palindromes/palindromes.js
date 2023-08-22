8d79b45c5b6cbe87f12449e255ae944e79f6825b
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-22 16:06:37 +0200

    Finished 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-};
+    if (newString === newString.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }  
+}
 
 // Do not edit below this line
 module.exports = palindromes;
